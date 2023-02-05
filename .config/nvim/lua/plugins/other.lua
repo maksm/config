cb04e21c5e930a25f27cd6c4b209e302662d8622
Maks Mržek <maks.mrzek@gmail.com>
2023-02-05 16:08:24 +0100

    disable neorg

--- .config/nvim/lua/plugins/other.lua
+++ .config/nvim/lua/plugins/other.lua
@@ -42,24 +42,24 @@ return {
       require("windows").setup()
     end,
   },
-  {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    opts = {
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.norg.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/notes",
-            },
-          },
-        },
-      },
-    },
-    dependencies = { { "nvim-lua/plenary.nvim" } },
-  },
+  --  {
+  --     "nvim-neorg/neorg",
+  --     build = ":Neorg sync-parsers",
+  --     opts = {
+  --       load = {
+  --        ["core.defaults"] = {}, -- Loads default behaviour
+  --        ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+  --        ["core.norg.dirman"] = { -- Manages Neorg workspaces
+  --         config = {
+  --           workspaces = {
+  --            notes = "~/notes",
+  --          },
+  --       },
+  --     },
+  --    },
+  --   },
+  --   dependencies = { { "nvim-lua/plenary.nvim" } },
+  --  },
   {
     "ldelossa/gh.nvim",
     dependencies = { "ldelossa/litee.nvim" },
